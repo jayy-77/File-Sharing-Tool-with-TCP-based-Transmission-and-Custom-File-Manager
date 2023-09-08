481f6d879ca5856e31dfd8c7d4970fcd8654c383
jay prajapati <79649559+jayy-77@users.noreply.github.com>
2023-09-08 16:33:15 +0530

    V:Disk =>  multiple disk selection

---
+++ front-end/file_explorer/src/components/Send.js
@@ -0,0 +1,72 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "bootstrap/dist/js/bootstrap.bundle.min";
+import axios from 'axios'
+import { useEffect, useState } from "react";
+
+function Send() {
+    const [structure, setStructure] = useState(null)
+    const [path, setPath] = useState("")
+    const [disks, setDisks] = useState([])
+    const URL = 'http://127.0.0.1:4544'
+
+    function directory_operation(item, end_point) {
+        axios.post(`${URL}/${end_point}`, { item_object: item })
+            .then(response => { 
+                setStructure(response.data); 
+                setPath(response.data[1]); 
+                setDisks(response.data[2])
+            })
+            .catch(error => console.log(error))
+    }
+
+    function change_disk(e){
+        axios.post(`${URL}/change-disk`, {disk: e.target.value})
+            .then(response => {
+                setStructure(response.data)
+                setPath(response.data[1])
+                setDisks(response.data[2])
+            })
+            .catch(err => console.log(err))
+    }
+
+    useEffect(() => {
+        axios.get('http://127.0.0.1:4544/directory', { directory: null })
+            .then(response => { 
+                setStructure(response.data); 
+                setPath(response.data[1]);
+                setDisks(response.data[2])
+            })
+            .catch(error => console.log(error))
+    }, [])
+
+    return (
+        <>
+            <div className="container">
+                <div className="mt-3 border-bottom border-primary d-flex">
+                    <img alt="none" src="back.png" className="p-3" onClick={() => directory_operation(null, 'back-dir')} />
+                    <div class="dropdown p-3 d-flex">
+                        <select class="form-select" onChange={(e) => change_disk(e)} >
+                            {
+                                disks && disks.map((d) => (
+                                    <option value={d} className="dropdown-item">Disk: {d}</option>
+                                ))
+                            }
+                        </select>
+                    <h5 className="m-2 text-light">{path}</h5>
+                    </div>
+                </div>
+
+                <div className="row row-cols-2 row-cols-lg-5" style={{ maxHeight: '800px', overflowY: 'auto' }}>
+                    {structure ? (
+                        structure[0].map((item) => (<div className="col p-3" onDoubleClick={() => directory_operation(item, 'post-data')}>
+                            <img className="justify-content-center" src={item["type"] === "directory" ? "folder.png" : "file.svg"} alt="none" />
+                            <h6 className="text-light">{item["content"]}</h6>
+                        </div>))
+                    ) : null}
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Send
---
+++ main.py
@@ -0,0 +1,85 @@
+from flask import Flask, request, jsonify
+import os
+import socket
+import time
+from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+url = "http://localhost:3000"
+file_name, file_size, host = "", "", ""
+def directory_finder(listdir):
+    root_struct = []
+
+    for i in listdir:
+        if os.path.isfile(f"{os.getcwd()}/{i}"):
+            root_struct.append({"type": "file", "content": i})
+        else:
+            root_struct.append({"type": "directory", "content": i})
+
+    return root_struct
+
+def explorer():
+    app = Flask(__name__)
+    CORS(app)
+
+    root_disks = []
+    for i in range(65, 91):
+        if os.path.isdir(f"{chr(i)}:/"):
+            root_disks.append(f"{chr(i)}:/")
+
+    root = root_disks[0]
+
+    @app.route("/directory")
+    def traverse():
+        os.chdir(root)
+        return jsonify(directory_finder(os.listdir()), os.getcwd(), root_disks)
+
+    @app.route("/change-disk", methods=["POST"])
+    def change_desk():
+        disk_name = request.get_json()
+        print(disk_name)
+        os.chdir(disk_name["disk"])
+        return jsonify(directory_finder(os.listdir()), os.getcwd(), root_disks)
+
+    @app.route("/post-data", methods=["POST"])
+    def change_dir():
+        global file_name, file_size, host
+        data = request.get_json()
+
+        if data["item_object"]["type"] == "directory":
+            os.chdir(os.getcwd() + "/" + data["item_object"]["content"])
+
+        else:
+            host = socket.gethostname()
+            port = 9970
+            client_socket = socket.socket()
+            client_socket.connect((host, port))
+
+            content = data["item_object"]["content"]
+
+            file_name = content.split("/")[-1]
+            file = open(content, "rb")
+            file_size = os.path.getsize(content)
+            info = f"{file_name}<SEP>{file_size}"
+            client_socket.send(info.encode())
+            file_data_byte = file.read()
+            client_socket.sendall(file_data_byte)
+            client_socket.send(b"<END>")
+            client_socket.close()
+            file.close()
+
+        return jsonify(directory_finder(os.listdir()), os.getcwd(), root_disks)
+
+    @app.route("/back-dir", methods=["POST"])
+    def back_dir():
+        os.chdir("..")
+        return jsonify(directory_finder(os.listdir()), os.getcwd(), root_disks)
+
+
+
+    if __name__ == "__main__":
+        app.run(host="0.0.0.0", port=4544)
+
+
+while True:
+    explorer()
